9757f047e13ff1bc81b97697d65bc1a6aca4a1df
Jimmy Truong <22500583+jimmywctruong@users.noreply.github.com>
2023-05-21 18:11:45 -0500

    Initial MVP

--- functions/order-tacos.ts
+++ functions/order-tacos.ts
@@ -1,15 +1,45 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import { MongoClient } from 'mongodb';
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  // your server-side functionality
+  try {
+    const { MONGODB_URI, MONGODB_DB } = process.env;
 
-  console.log(event);
-  console.log(context);
+    if (!MONGODB_URI || !MONGODB_DB) {
+      throw new Error('Missing environment variables for MongoDB connection');
+    }
 
-  return {
-    statusCode: 200,
-    body: 'success'
+    const client = await MongoClient.connect(MONGODB_URI);
+    const db = client.db(MONGODB_DB);
+
+    const formResponse = JSON.parse(event.body)
+    const cleanResponse = copyObjectWithoutNullValues(formResponse)
+    console.log(cleanResponse)
+
+    const collection = db.collection('orders');
+    await collection.insertOne(cleanResponse);
+
+    client.close();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Data inserted successfully' }),
+    };
+  } catch (error) {
+
+    console.log(error.message)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
   }
 };
+
+function copyObjectWithoutNullValues<T extends Record<string, any>>(obj: T): Partial<T> {
+  return Object.fromEntries(
+    Object.entries(obj)
+      .filter(([_, value]) => value !== null)
+  );
+}
 
 export { handler };
--- src/TacoForm.js
+++ src/TacoForm.js
@@ -3,7 +3,14 @@ import React, { useState } from 'react';
 const TacoForm = () => {
   const [formData, setFormData] = useState({
     name: '',
-    email: '',
+    asadaCount: null,
+    alpastorCount: null,
+    chorizoCount: null,
+    polloCount: null,
+    chicharronCount: null,
+    barbacoaCount: null,
+    cabesaCount: null,
+    tripasCount: null,
   });
 
   const handleChange = (e) => {
@@ -36,25 +43,118 @@ const TacoForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        Name:
+        First and Last Name:
         <input
           type="text"
           name="name"
           value={formData.name}
           onChange={handleChange}
+          required
         />
       </label>
       <br />
       <label>
-        Email:
+        Asada (Steak):
+        <input 
+          type="number"
+          name="asada-count"
+          value={formData.asadaCount}
+          onChange={handleChange}
+        />
+      </label>
+      <br/>
+      <label>
+        Al-Pastor (Pork):
         <input
-          type="email"
-          name="email"
-          value={formData.email}
+          type="number"
+          name="alpastor-count"
+          value={formData.alpastorCount}
           onChange={handleChange}
         />
       </label>
       <br />
+      <label>
+        Chorizo (Pork):
+        <input 
+          type="number"
+          name="chorizo-count"
+          value={formData.chorizoCount}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Pollo (Pork):
+        <input 
+          type="number"
+          name="pollo-count"
+          value={formData.polloCount}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Chicharron (Pork Skin):
+        <input 
+          type="number" 
+          name="chicharron-count"
+          value={formData.chicharronCount}
+          onChange={handleChange}
+        />
+        
+      </label>
+      <br />
+      <label>
+        Barbacoa:
+        <input
+          type="number"
+          name="barbacoa-count"
+          value={formData.barbacoaCount}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Cabesa:
+        <input
+          type="number"
+          name="cabesa-count"
+          value={formData.cabesaCount}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Tripas:
+        <input 
+          type="number"
+          name="tripas-count"
+          value={formData.tripasCount}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      {/* <fieldset>
+        <legend>Menudo</legend>
+        <input type="checkbox" name="grid1" id="menduo" value="Yes" />
+        <label for="menduo">Yes</label>
+        <input type="radio" name="grid2" id="large" value="Large" />
+        <label for="large">Large</label>
+        <input type="radio" name="grid3" id="small" value="small" />
+        <label for="small">Small</label>
+      </fieldset>
+      <fieldset>
+        <label for="quesadilla">Quesadilla:</label>
+        <input type="checkbox" name="grid4" id="enablequesadilla" value="Yes" />
+        <label for="enablequesadilla">Yes</label>
+        <select name="quesadilla" id="quesadilla">
+            <option value="asada">Asada (Steak)</option>
+            <option value="alpastor">Al-Pastor</option>
+            <option value="chorizo">Chorizo</option>
+            <option value="chicharron">Chicharron</option>
+            <option value="barbacoa">Barbacoa</option>
+        </select>
+      </fieldset> */}
       <button type="submit">Submit</button>
     </form>
   );
