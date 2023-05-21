b79091e6f80dbd83a179e216d786312464dad301
Jimmy Truong <22500583+jimmywctruong@users.noreply.github.com>
2023-05-21 17:13:14 -0500

    Use ajax instead of netlify form detection

--- src/App.js
+++ src/App.js
@@ -1,77 +1,13 @@
 import React from 'react';
 import './App.css';
+import TacoForm from './TacoForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Miguel's Direct Messages</h1>
-        <form name="tacos" method="POST">
-          <label>
-            First and Last Name:
-            <input type="text" name="name" />
-          </label>
-
-          {/* <label>
-            Asada (Steak):
-            <input type="number" name="asada-qty" />
-          </label>
-          <label>
-            Al-Pastor (Pork):
-            <input type="number" name="alpastor-qty" />
-          </label>
-          <label>
-            Chorizo (Pork):
-            <input type="number" name="chorizo-qty" />
-          </label>
-          <label>
-            Pollo (Pork):
-            <input type="number" name="pollo-qty" />
-          </label>
-          <label>
-            Chicharron (Pork Skin):
-            <input type="number" name="chicharron-qty" />
-          </label>
-          <label>
-            Barbacoa:
-            <input type="number" name="barbacoa-qty" />
-          </label>
-          <label>
-            Cabesa:
-            <input type="number" name="cabesa-qty" />
-          </label>
-          <label>
-            Tripas:
-            <input type="number" name="tripas-qty" />
-          </label>
-
-          <fieldset>
-            <legend>Menudo</legend>
-            <input type="checkbox" name="grid1" id="menduo" value="Yes" />
-            <label for="menduo">Yes</label>
-            <input type="radio" name="grid2" id="large" value="Large" />
-            <label for="large">Large</label>
-            <input type="radio" name="grid3" id="small" value="small" />
-            <label for="small">Small</label>
-          </fieldset>
-
-          <fieldset>
-            <label for="quesadilla">Quesadilla:</label>
-            <input type="checkbox" name="grid4" id="enablequesadilla" value="Yes" />
-            <label for="enablequesadilla">Yes</label>
-            <select name="quesadilla" id="quesadilla">
-              <option value="asada">Asada (Steak)</option>
-              <option value="alpastor">Al-Pastor</option>
-              <option value="chorizo">Chorizo</option>
-              <option value="chicharron">Chicharron</option>
-              <option value="barbacoa">Barbacoa</option>
-            </select>
-          </fieldset> */}
-
-          <input type="submit" value="Submit" />
-
-          <input type="hidden" name="form-name" value="tacos"></input>
-        </form>
+        <TacoForm/>
       </header>
     </div>
   );
---
+++ src/TacoForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const TacoForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform AJAX request using formData
+    // Replace the URL with your own endpoint
+    fetch('/.netlify/functions/order-tacos', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        // Handle success case here
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Handle error case here
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default TacoForm;
