1749bce99cbe85dd2b1160ff57246aaecf6e8c07
Jimmy Truong <22500583+jimmywctruong@users.noreply.github.com>
2023-05-21 16:41:55 -0500

    Use unique form input names

--- src/App.js
+++ src/App.js
@@ -34,30 +34,30 @@ function App() {
           </label>
           <label>
             Barbacoa:
-            <input type="number" name="name" />
+            <input type="number" name="barbacoa-qty" />
           </label>
           <label>
             Cabesa:
-            <input type="number" name="name" />
+            <input type="number" name="cabesa-qty" />
           </label>
           <label>
             Tripas:
-            <input type="number" name="name" />
+            <input type="number" name="tripas-qty" />
           </label>
 
           <fieldset>
             <legend>Menudo</legend>
-            <input type="checkbox" name="grid" id="menduo" value="Yes" />
+            <input type="checkbox" name="grid1" id="menduo" value="Yes" />
             <label for="menduo">Yes</label>
-            <input type="radio" name="grid" id="large" value="Large" />
+            <input type="radio" name="grid2" id="large" value="Large" />
             <label for="large">Large</label>
-            <input type="radio" name="grid" id="small" value="small" />
+            <input type="radio" name="grid3" id="small" value="small" />
             <label for="small">Small</label>
           </fieldset>
 
           <fieldset>
             <label for="quesadilla">Quesadilla:</label>
-            <input type="checkbox" name="grid" id="enablequesadilla" value="Yes" />
+            <input type="checkbox" name="grid4" id="enablequesadilla" value="Yes" />
             <label for="enablequesadilla">Yes</label>
             <select name="quesadilla" id="quesadilla">
               <option value="asada">Asada (Steak)</option>
